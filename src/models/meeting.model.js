70596de1d009a8962ff1960a52ce5ba3c8872336
yashi-1311 <yashigupta1101@gmail.com>
2025-10-18 15:46:27 +0530

    Update meeting.model.js

--- src/models/meeting.model.js
+++ src/models/meeting.model.js
@@ -1,1 +1,16 @@
 
+import mongoose, { Schema } from "mongoose";
+
+
+const meetingSchema = new Schema(
+    {
+        user_id: { type: String },
+        meetingCode: { type: String, required: true },
+        date: { type: Date, default: Date.now, required: true }
+    }
+)
+
+const Meeting = mongoose.model("Meeting", meetingSchema);
+
+export { Meeting };
+
